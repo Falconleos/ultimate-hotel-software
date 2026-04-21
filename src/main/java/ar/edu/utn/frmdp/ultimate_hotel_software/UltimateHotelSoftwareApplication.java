43812cc3be41c8ad92770598e9883bfd4c134453
Falconleos <leonelfsotopublic@gmail.com>
2026-04-21 10:36:50 -0300

    chore: estructura inicial del proyecto spring boot

---
+++ src/main/java/ar/edu/utn/frmdp/ultimate_hotel_software/UltimateHotelSoftwareApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frmdp.ultimate_hotel_software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateHotelSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimateHotelSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/utn/frmdp/ultimate_hotel_software/UltimateHotelSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frmdp.ultimate_hotel_software;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateHotelSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
