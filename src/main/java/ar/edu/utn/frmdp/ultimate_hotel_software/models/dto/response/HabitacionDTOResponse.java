e61ae35b4175fdf0b279dcdfb320622445fcaeaf
Leonel Federico Soto <leonelfsotopublic@gmail.com>
2026-06-09 22:12:24 -0300

    correccion mapeado de habitacion para compilar

--- src/main/java/ar/edu/utn/frmdp/ultimate_hotel_software/models/dto/response/HabitacionDTOResponse.java
+++ src/main/java/ar/edu/utn/frmdp/ultimate_hotel_software/models/dto/response/HabitacionDTOResponse.java
@@ -2,12 +2,18 @@ package ar.edu.utn.frmdp.ultimate_hotel_software.models.dto.response;
 
 import ar.edu.utn.frmdp.ultimate_hotel_software.enums.EstadoHabitacion;
 import ar.edu.utn.frmdp.ultimate_hotel_software.enums.TipoHabitacion;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class HabitacionDTOResponse {
+    private Long id;
+
     private Integer numero;
 
     private TipoHabitacion tipo;
